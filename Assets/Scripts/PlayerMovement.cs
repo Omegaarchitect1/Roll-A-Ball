77b51e659348f1626afc6ca1e3f2d3b486af1a60
Omegaarchitect1 <omegaarchitect1@gmail.com>
2018-09-16 20:30:53 -0500

    Roll A Ball Project from Class

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+
+    private Rigidbody rb;
+    private int count;
+    public Text WinText;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+    void FixedUpdate()
+    {
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        float movementVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup")) 
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count:" + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "You Win";
+        }
+    }
+ 
+}
